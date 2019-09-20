43a294f8841abc3e21bb03c8850d4a5e20032742
Swapnil <pswapnil05@gmail.com>
2019-09-20 18:13:01 +0530

    17th Sept 2019

---
+++ String_17th Sept/String2Primitive.java
@@ -0,0 +1,14 @@
+package com.java.strings;
+
+public class String2Primitive {
+
+  public static void main(String[] args) {
+    String s = "JAVA";
+    String p = "JAVA";
+    Integer i = 10;
+    Integer j = 10;
+    j++;
+    System.out.println(s.equals(p));
+    System.out.println(s + p);
+  }
+}
